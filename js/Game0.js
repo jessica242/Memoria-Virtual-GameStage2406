ad39bb395fd861f6c54d60af23f56bd98301a1ed
jessica242 <js61907@gmail.com>
2017-06-26 14:02:25 -0300

    Penultimos acertos
    Tudo se move, game state e cronometro funcionando, novas atividades

---
+++ js/Game0.js
@@ -0,0 +1,27 @@
+var Game0 = function() {};
+
+Game0.prototype = {
+    
+    preload: function() {
+        
+        game.load.image('pginicial', 'Imagens/marca.png');
+        game.load.image('botao',  'Imagens/botao.png');  
+        
+    }, 
+        
+    create: function() {
+        
+        game.add.sprite(0, 0, 'pginicial');
+        this.playButton = this.game.add.button(500,400, 'botao', this.playTheGame, this);
+        
+    },
+        
+    update: function() {
+        
+    },
+    
+    playTheGame: function (button) {
+        game.state.start("Fase 1");        
+    }
+    
+};
